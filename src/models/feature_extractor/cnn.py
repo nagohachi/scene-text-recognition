19a67d7afe4dd7274e4af6bca6b55d8d4c414d84
yukihira-pot <r.magoshi.ma54@gmail.com>
2026-01-01 17:59:38 +0900

    refactor

--- src/models/feature_extractor/cnn.py
+++ src/models/feature_extractor/cnn.py
@@ -2,14 +2,26 @@ import torch
 import torch.nn as nn
 
 
+class PreNormConv(nn.Module):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1
+    ) -> None:
+        super().__init__()
+        padding = kernel_size // 2
+        self.norm = nn.BatchNorm2d(in_channels)
+        self.relu = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conv(self.relu(self.norm(x)))
+
+
 class BasicBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, (3, 3), stride=1, padding=1)
-        self.norm1 = nn.BatchNorm2d(in_channels)
-        self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(out_channels, out_channels, (3, 3), stride=1, padding=1)
-        self.dim_match = (
+        self.pre_norm_conv1 = PreNormConv(in_channels, out_channels, kernel_size=3)
+        self.pre_norm_conv2 = PreNormConv(out_channels, out_channels, kernel_size=3)
+        self.shortcut = (
             nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, (1, 1)),
                 nn.BatchNorm2d(out_channels),
@@ -17,21 +29,15 @@ class BasicBlock(nn.Module):
             if in_channels != out_channels
             else nn.Identity()
         )
-        self.norm2 = nn.BatchNorm2d(out_channels)
 
     def forward(
         self, x: torch.Tensor, xlens: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
         x_residual = x
-        x = self.norm1(x)
-        x = self.relu(x)
-        x = self.conv1(x)
+        x = self.pre_norm_conv1(x)
+        x = self.pre_norm_conv2(x)
 
-        x = self.norm2(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-
-        x = x + self.dim_match(x_residual)
+        x = x + self.shortcut(x_residual)
         return (x, xlens)
 
 
