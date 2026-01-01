083f35519f37c1866c7c2907051fddd7b5c20594
yukihira-pot <r.magoshi.ma54@gmail.com>
2026-01-01 19:46:10 +0900

    added transformer encoder

---
+++ src/models/encoder/transformer.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+from src.models.utils import lens_to_mask
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, hidden_size: int, num_heads: int, num_layers: int) -> None:
+        self.hidden_size = hidden_size
+        encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=num_heads)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+
+    def forward(
+        self, x: torch.Tensor, xlens: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        padding_mask = lens_to_mask(xlens)
+        x = self.encoder.forward(x, src_key_padding_mask=padding_mask)
+        return x, xlens
