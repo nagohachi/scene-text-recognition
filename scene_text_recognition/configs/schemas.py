75a210dd57cd4c7ff9fcc601617c8c75dd3147e7
yukihira-pot <r.magoshi.ma54@gmail.com>
2026-01-01 22:41:42 +0900

    added schemas

---
+++ scene_text_recognition/configs/schemas.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class FeatureExtractorConfig:
+    type: Literal["resnet18"]
+    in_channels: int = 3
+
+
+@dataclass
+class EncoderConfig:
+    type: Literal["transformer"]
+    hidden_size: int
+    num_heads: int
+    num_layers: int
+
+
+@dataclass
+class CTCDecoderConfig:
+    blank_id: int
+    padding_id: int
+    reduction: Literal["mean", "sum", "none"] = "mean"
+    zero_infinity: bool = True
+
+
+@dataclass
+class CTCModelConfig:
+    feature_extractor: FeatureExtractorConfig
+    encoder: EncoderConfig
+    decoder: CTCDecoderConfig
+    vocab_size: int
