cf439f0a71e7592435545355cbe0987491211a35
yukihira-pot <r.magoshi.ma54@gmail.com>
2026-01-01 22:41:33 +0900

    modified ctc model

--- scene_text_recognition/frameworks/ctc.py
+++ scene_text_recognition/frameworks/ctc.py
@@ -4,23 +4,47 @@ import torch
 import torch.nn as nn
 from einops import rearrange
 
+from scene_text_recognition.configs.schemas import CTCModelConfig
 from scene_text_recognition.frameworks.schemas import CTCOutput
 from scene_text_recognition.models.decoder.ctc import CTCDecoder
-from scene_text_recognition.models.encoder.schamas import EncoderBase
-from scene_text_recognition.models.feature_extractor.schemas import FeatureExtractorBase
+from scene_text_recognition.models.encoder.transformer import TransformerEncoder
+from scene_text_recognition.models.feature_extractor.cnn import ResnetV2FeatureExtractor
 
 
 class CTCModel(nn.Module):
-    def __init__(
-        self,
-        feature_extractor: FeatureExtractorBase,
-        encoder: EncoderBase,
-        ctc_decoder: CTCDecoder,
-    ) -> None:
+    def __init__(self, config: CTCModelConfig) -> None:
         super().__init__()
-        self.feature_extractor = feature_extractor
-        self.encoder = encoder
-        self.ctc_decoder = ctc_decoder
+        self.config = config
+
+        match config.feature_extractor.type:
+            case "resnet18":
+                self.feature_extractor = ResnetV2FeatureExtractor(
+                    in_channels=config.feature_extractor.in_channels,
+                    type="resnet18",
+                )
+            case _:
+                raise ValueError(
+                    f"Unknown feature extractor: {config.feature_extractor.type}"
+                )
+
+        match config.encoder.type:
+            case "transformer":
+                self.encoder = TransformerEncoder(
+                    hidden_size=config.encoder.hidden_size,
+                    num_heads=config.encoder.num_heads,
+                    num_layers=config.encoder.num_layers,
+                )
+            case _:
+                raise ValueError(f"Unknown encoder: {config.encoder.type}")
+
+        self.ctc_decoder = CTCDecoder(
+            blank_id=config.decoder.blank_id,
+            padding_id=config.decoder.padding_id,
+            reduction=config.decoder.reduction,
+            input_size=config.encoder.hidden_size,
+            output_size=config.vocab_size,
+            zero_infinity=config.decoder.zero_infinity,
+        )
 
     def forward(
         self,
